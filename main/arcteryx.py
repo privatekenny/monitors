c0a9b266a768c0098c4cf24485f84b1fc55ed2ae
privateken <kenny_d26@yahoo.com>
2022-03-31 19:52:20 -0700

    fix import

--- main/arcteryx.py
+++ main/arcteryx.py
@@ -3,8 +3,6 @@ import json
 import requests
 from multiprocessing import Process
 from threading import Thread, current_thread
-from tweet import *
-from discord import *
 import time
 import sys
 import os
