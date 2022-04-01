a2332ee2ac7583d67f19ef311cbb32a096c248c1
privateken <kenny_d26@yahoo.com>
2022-03-31 19:39:32 -0700

    fixing status enum and threads

--- models/Product.py
+++ models/Product.py
@@ -7,12 +7,4 @@ class Product:
         self.image = image
 
     def get_product(self):
-        product_dict = {
-            "name": self.name,
-            "size": self.size,
-            "price": self.price,
-            "link": self.link,
-            "image": self.image
-        }
-
-        return product_dict
+        return self.__dict__
